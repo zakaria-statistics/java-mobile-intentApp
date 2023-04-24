7738f73e5ae6372ba538f475a73c2d871fdfe7fe
zakaria-statistics <65316874+zakaria-statistics@users.noreply.github.com>
2023-04-24 23:52:35 +0100

    first commit

---
+++ app/src/main/java/ma/enset/intentapp/IntentActivity.java
@@ -0,0 +1,14 @@
+package ma.enset.intentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intent);
+    }
+}

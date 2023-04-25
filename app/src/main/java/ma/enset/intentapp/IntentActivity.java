76653eb813bde37c4bfff5299f561936c36007ed
zakaria-statistics <65316874+zakaria-statistics@users.noreply.github.com>
2023-04-25 23:40:32 +0100

    second commit

--- app/src/main/java/ma/enset/intentapp/IntentActivity.java
+++ app/src/main/java/ma/enset/intentapp/IntentActivity.java
@@ -2,7 +2,10 @@ package ma.enset.intentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class IntentActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class IntentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intent);
+
+        TextView textView1= findViewById(R.id.textView4);
+        TextView textView2= findViewById(R.id.textView5);
+
+        Bundle bundle= new Bundle();
+        String username= bundle.getString("editTextUsername");
+        String password= bundle.getString("editTextPassword");
+        String tvUsername= bundle.getString("textViewUsername");
+        String tvPassword= bundle.getString("textViewPassword");
+
+        textView1.setText(tvUsername+username);
+        textView2.setText(tvPassword+password);
+
+    }
+    public void back(View view){
+        Intent myIntent=new Intent(this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
--- app/src/main/java/ma/enset/intentapp/MainActivity.java
+++ app/src/main/java/ma/enset/intentapp/MainActivity.java
@@ -2,7 +2,11 @@ package ma.enset.intentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void submit(View view){
+        TextView textViewUsername= findViewById(R.id.textView1);
+        TextView textViewPassword= findViewById(R.id.textView2);
+        EditText editTextUsername= findViewById(R.id.editText);
+        EditText editTextPassword= findViewById(R.id.editText2);
+
+        Intent myIntent=new Intent(this, IntentActivity.class);
+        Bundle bundle=new Bundle();
+
+        bundle.putString("editTextUsername", editTextUsername.getText().toString());
+        bundle.putString("editTextPassword", editTextPassword.getText().toString());
+        bundle.putString("textViewUsername", textViewUsername.getText().toString());
+        bundle.putString("textViewPassword", textViewPassword.getText().toString());
+        myIntent.putExtras(bundle);
+        startActivity(myIntent);
     }
 }
